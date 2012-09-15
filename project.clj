c5be4e27062014ce1133c4f2385f69b2242fde6e
David Petrovics <dpetrovics@gmail.com>
2012-09-15 11:20:47 -0400

    bumped version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject web-app-starter/lein-template "0.1.0-SNAPSHOT"
+(defproject web-app-starter/lein-template "0.2.0-SNAPSHOT"
   :description "Lein template for web app using Noir, CouchDB, Enlive, Forms-Bootstrap, with basic model and view namespaces, html and css."
   :url "https://github.com/dpetrovics/web-app-starter"
   :license {:name "Eclipse Public License"
